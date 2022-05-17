38e466ef1ca5ab173c57f2adb313fc3db1ca3cbb
pesz <peter.szarvas@softwarepark.cc>
2022-05-17 15:01:22 +0200

    feat: chapter 3 null-handling

---
+++ chapter-3/null-handling.fs
@@ -0,0 +1,29 @@
+open System
+
+let nullObj:string = null
+let nullPri = Nullable<int>()
+
+let fromNullObj = Option.ofObj nullObj
+let fromNullPri = Option.ofNullable nullPri
+
+let toNullObj = Option.toObj fromNullObj
+let toNullPri = Option.toNullable fromNullPri
+
+
+let resultPM input = 
+    match input with
+    | Some value -> value
+    | None -> "------"
+
+
+let result = Option.defaultValue "------" fromNullObj
+
+let result2 = resultPM fromNullObj
+
+//C# ?? operator is nicer ^^
+
+
+let setDefault = Option.defaultValue "<default>"
+
+
+let resultWithSetDefault = fromNullObj |> setDefault
