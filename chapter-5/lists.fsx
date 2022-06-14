9a324f1c047cc07c64bf0736aa92dcd89fc94d0c
pesz <peter.szarvas@softwarepark.cc>
2022-06-14 15:18:53 +0200

    chore: completed Lists

--- chapter-5/lists.fsx
+++ chapter-5/lists.fsx
@@ -28,3 +28,53 @@ let getEvens items =
 
 
 let evens = getEvens myList
+
+
+let triple items =
+    items
+    |> List.map (fun x -> x * 3)
+
+
+triple items
+
+let quantityWithPrices = [(1,0.25M);(5,0.25M);(1,2.25M);(1,125M);(7,10.9M)]
+
+let sum items = 
+    items
+    |> List.map (fun (quantity,price) -> decimal quantity * price)
+    |> List.sum
+
+
+let total = sum quantityWithPrices
+
+let sum_2 items =
+    items
+    |> List.sumBy (fun (q,p)->decimal q * p)
+
+let total2= sum_2 quantityWithPrices
+
+let getTotal items =
+    items
+    |> List.fold (fun acc (q,p)-> acc + decimal q * p) 0M
+
+
+let total_withFold = getTotal quantityWithPrices
+
+let numbers = [1;2;3;4;5;7;6;5;4;3]
+
+let gbResult items = 
+    items
+    |> List.groupBy (fun x -> x)
+
+let uniqueItems items =
+    items
+    |> List.map (fun (i,_) -> i)
+
+let unResult = 
+    numbers
+    |> gbResult
+    |> uniqueItems
+
+
+// Set will sort items if it can
+
