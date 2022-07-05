f8619e4cdec2d5e0b36136374fb30b817233bcf3
pesz <peter.szarvas@softwarepark.cc>
2022-07-05 07:45:50 +0200

    chore: chapter 6 - read file

---
+++ chapter-6/Program.fs
@@ -0,0 +1,21 @@
+﻿open System.IO
+
+let readFile path =
+    try
+        seq {
+            use reader = new StreamReader(File.OpenRead(path));
+            while not reader.EndOfStream do
+                reader.ReadLine()
+        }
+        |> Ok
+    with
+    | ex -> Error ex
+
+
+let import path =
+    match path |> readFile with
+    | Ok r -> r |>  Seq.iter (fun x -> printfn "%A" x)
+    | Error ex -> printfn "Error: %A" ex.Message
+
+let path = Path.Combine(__SOURCE_DIRECTORY__,"resources", "customers.csv")
+import path
