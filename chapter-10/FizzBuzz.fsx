fde453d448997d9c1bb42165d5f7992d966d2450
pesz <peter.szarvas@softwarepark.cc>
2022-07-26 12:48:05 +0200

    chore: upcasting vs factory

--- chapter-10/FizzBuzz.fsx
+++ chapter-10/FizzBuzz.fsx
@@ -1,13 +1,27 @@
+type IFizzBuzz =
+    abstract member Calculate : int -> string
+
+type NoopFizzBuzz() =
+    interface IFizzBuzz with
+        member _.Calculate(value) = string value
+
 type FizzBuzz(mapping) =
     let calculate n =
         mapping
         |> List.map (fun (v,s) -> if n % v = 0 then s else "")
         |> List.reduce (+)
         |> fun s -> if s <> "" then s else string n
-        
-    member _.Calculate(value) = calculate value
+
+    interface IFizzBuzz with
+        member _.Calculate(value) = calculate value
+
+let fizzBuzzFactory name : IFizzBuzz =
+    if name = "first" then FizzBuzz([(3,"Fizz");(5,"Buzz")]) else NoopFizzBuzz()
 
 let doFizzBuzz =
-    let fizzBuzz = FizzBuzz([(3,"Fizz");(5,"Buzz")])
+    //with factory
+    // let fizzBuzz = fizzBuzzFactory "first_2"
+    //with upcasting
+    let fizzBuzz = FizzBuzz([(3,"Fizz");(5,"Buzz")]) :> IFizzBuzz
     [1..15]
     |> List.map fizzBuzz.Calculate
